1aa4cd88b78fa98ae9a5b9de62c4b3454adc341b
jdavidagudelo <jdaaa2009@gmail.com>
2017-03-30 10:19:44 -0500

    Fixing some issues.

--- social_auth/backends/contrib/belgiumeid.py
+++ social_auth/backends/contrib/belgiumeid.py
@@ -1,1 +1,22 @@
-from social.backends.belgiumeid import BelgiumEIDOpenId as EIDBackend
+from social_auth.backends import OpenIDBackend, OpenIdAuth
+
+E_ID_OPENID_URL = 'https://www.e-contract.be/eid-idp/endpoints/openid/auth'
+
+
+class EIDBackend(OpenIDBackend):
+    """e-ID OpenID authentication backend"""
+    name = 'eID'
+
+
+class EIDAuth(OpenIdAuth):
+    """Belgium e-ID OpenID authentication"""
+    AUTH_BACKEND = EIDBackend
+
+    def openid_url(self):
+        """Return Belgium e-ID OpenID service url"""
+        return E_ID_OPENID_URL
+
+# Backend definition
+BACKENDS = {
+    'eID': EIDAuth,
+}
--- social_auth/backends/contrib/vkontakte.py
+++ social_auth/backends/contrib/vkontakte.py
@@ -1,9 +1,4 @@
 import warnings
 
-from social.backends.vk import VKontakteOpenAPI as VKOpenAPIBackend, \
-                               VKOAuth2 as VKOAuth2Backend, \
-                               VKAppOAuth2 as VKApplicationBackend
-
-warnings.warn('Vkontakte backend was renamed to vk backend, '
-              'settings were renamed too. Please adjust your '
-              'settings', DeprecationWarning)
+warnings.warn('Vkontakte backend was renamed to vk backend, settings were renamed too. Please adjust your settings', DeprecationWarning)
+from .vk import *
